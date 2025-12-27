9036edd85a01040f4d19b52293a37d7bced4e057
Butchxdiabolical <pm408686@gmail.com>
2025-12-28 01:11:24 +0530

    Add PROJECT1.py

---
+++ PROJECT1.py
@@ -0,0 +1,21 @@
+x=int(input("enter first number:"))
+y=int(input("enter second number:"))
+
+sum=x+y
+diff=x-y
+prod=x*y
+quot=x/y
+
+print ("Addition:",sum)
+print("Subtraction:",diff)
+print("Multiplication:",prod)
+print("Division:",quot)
+
+
+
+
+
+
+
+
+
